759bb2eb44d9af0ef0569bb7bf081d289fc82df5
louis17782 <luissanteliz22@gmail.com>
2024-08-13 16:32:35 -0400

    add new modal

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
 import './style.scss'
 
+//modal
+const modal = document.getElementById("myModal");
+const span = document.getElementsByClassName("close")[0];
+const button = document.getElementById("aceptar");
+
+window.onload = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+button.onclick = function () {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
 
