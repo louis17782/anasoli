2ee042f8d311c23549f8f140d60e79d712aa66d3
louis17782 <luissanteliz22@gmail.com>
2024-08-14 10:25:27 -0400

    add new function for hidden

--- main.js
+++ main.js
@@ -20,7 +20,36 @@ window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
-}
+};
+
+//animations
+
+document.addEventListener('DOMContentLoaded', function () {
+  // Removemos la clase 'no-transition' para activar las transiciones solo después de que la página ha cargado
+  const hiddenElement = document.querySelectorAll('.hidden');
+  hiddenElement.forEach(element => {
+    element.classList.add('no-transition')
+  });
+
+  // Configuramos el Intersection Observer
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('visible');
+        observer.unobserve(entry.target); // Deja de observar el elemento después de que se ha animado
+      }
+    });
+  }, {
+    threshold: 0.1,
+    rootMargin: '0px 0px -50px 0px'
+  });
+
+  // Observamos el elemento con el ID 'hidden'
+  hiddenElement.forEach(element => {
+    observer.observe(element);
+  });
+});
+
 
 
 
