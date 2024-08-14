cc44d6165d4852ecbbc1620546a0ab28f53044d1
louis17782 <luissanteliz22@gmail.com>
2024-08-14 16:49:59 -0400

    fix function modal

--- main.js
+++ main.js
@@ -2,18 +2,15 @@ import './style.scss'
 
 //modal
 const modal = document.getElementById("myModal");
-const span = document.getElementsByClassName("close")[0];
-const button = document.getElementById("aceptar");
+const span = document.getElementById("close");
+const ig = document.getElementById("igbutton");
 
-window.onload = function() {
+ig.onclick = function() {
   modal.style.display = "block";
-}
+};
 
 span.onclick = function() {
   modal.style.display = "none";
-}
-button.onclick = function () {
-    modal.style.display = "none";
 }
 
 window.onclick = function(event) {
@@ -21,22 +18,19 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 };
-
 //animations
 
 document.addEventListener('DOMContentLoaded', function () {
-  // Removemos la clase 'no-transition' para activar las transiciones solo después de que la página ha cargado
   const hiddenElement = document.querySelectorAll('.hidden');
   hiddenElement.forEach(element => {
     element.classList.add('no-transition')
   });
 
-  // Configuramos el Intersection Observer
   const observer = new IntersectionObserver((entries, observer) => {
     entries.forEach(entry => {
       if (entry.isIntersecting) {
         entry.target.classList.add('visible');
-        observer.unobserve(entry.target); // Deja de observar el elemento después de que se ha animado
+        observer.unobserve(entry.target);
       }
     });
   }, {
@@ -44,7 +38,6 @@ document.addEventListener('DOMContentLoaded', function () {
     rootMargin: '0px 0px -50px 0px'
   });
 
-  // Observamos el elemento con el ID 'hidden'
   hiddenElement.forEach(element => {
     observer.observe(element);
   });
